9d837fee51b84b1418e1884a640e49894e761697
AlohaYiming <aloha_xdy@163.com>
2018-04-10 22:39:59 +0800

    add

--- scripts/FaceRecognition.py
+++ scripts/FaceRecognition.py
@@ -20,11 +20,6 @@ def callback(imgmsg):
 
 def init():
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
     rospy.init_node('listener', anonymous=True)
     rospy.Subscriber("/find_face/img", Image, callback)
     
