ac5a5de64dcb07d9c14f91f711bb62040ea753f5
AlohaYiming <aloha_xdy@163.com>
2018-04-10 22:32:40 +0800

    add

--- scripts/run_tensorflow.py
+++ scripts/run_tensorflow.py
@@ -9,11 +9,6 @@ def callback(imgmsg):
 
 def listener():
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
     rospy.init_node('listener', anonymous=True)
     rospy.Subscriber("/find_face/img", Image, callback)
     # spin() simply keeps python from exiting until this node is stopped
